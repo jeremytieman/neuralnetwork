6a6bb5895be1ca780ec193aea508f5909299eb57
Jeremy Tieman <jotieman@microsoft.com>
2019-04-19 17:59:27 -0700

    Implemented add operator

--- src/AddOper.cpp
+++ src/AddOper.cpp
@@ -7,27 +7,19 @@ namespace CodexMachina::NeuralNetwork
   template<class T>
   Tensor<T> AddOper<T>::process(const Tensor<T>& input1, const Tensor<T>& input2)
   {
-    if (input1.Rank() != input2.Rank())
-    {
-      std::stringstream ss{ "Ranks of inputs do not match. Input1 rank: " };
-      ss << input1.Rank() << " Input2 rank: " << input2.Rank() << "\n";
-      throw std::invalid_argument(ss.str());
-    }
-
     {
       const auto& dims1 = input1.Dimensions();
       const auto& dims2 = input2.Dimensions();
-
-      for (std::size_t i = 0; i < input1.Rank(); ++i)
-      {
-        if (dims1[i] != dims2[i])
-        {
-          std::stringstream ss{ "Dimensions at index " };
-          ss << i << " do not match. Dimension 1: " << dims1[i];
-          ss << " Dimension 2: " << dims2[i] << "\n";
-          throw std::invalid_argument(ss.str());
-        }
-      }
+      if (dims1 != dims2) throw std::invalid_argument("Dimensions of inputs do not match.");
     }
+
+    const auto& outputDims = input1.Dimensions();
+    const auto& inputData1 = input1.Data();
+    const auto& inputData2 = input2.Data();
+    std::vector<T> outputData;
+    outputData.reserve(inputData1.size());
+    for (std::size_t i = 0; i < inputData1.size(); ++i)
+      outputData.push_back(inputData1[i] + inputData2[i]);
+    return Tensor<T>(outputDims, outputData);
   }
 }
